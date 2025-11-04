fa26dba02230ee1e95ae8b5316c68c31f8f8221b
kavindaNwN <kavindanwn@gmail.com>
2025-11-04 09:57:23 +0530

    bitrate

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>Bitrae - Travel & Tour Booking Agency</title>
+<title>Bitrate - Travel & Tour Booking Agency</title>
 <meta name="author" content="Novara">
 <meta name="description" content="Novara - Travel & Tour Booking Agency">
 <meta name="keywords" content="Novara - Travel & Tour Booking Agency">
